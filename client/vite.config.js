5853f20c0b8e62f085fcb47dd7043eecd8d02914
Hosea <karanja.mungai23@gmail.com>
2025-02-02 21:09:35 +0300

    MINOR FIX

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  host: true,
-  strictPort: true,
-  port: 8000,
+  host: '0.0.0.0',
+   port: 3000,
+  allowedHosts: ['karanja-s-online-shop-front-end.onrender.com'],
+  
   },
 })
