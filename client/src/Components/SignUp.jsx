0aeec1d1abdc19e06df0919b6c7d5a83170ac47c
Hosea <karanja.mungai23@gmail.com>
2025-02-02 22:25:04 +0300

    MINOR FIX

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -40,7 +40,7 @@ function SignUp(){
             email,
             password
         }; 
-        fetch ('/user', {
+        fetch ('https://karanja-s-online-shop-v1q7.onrender.com/user', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
