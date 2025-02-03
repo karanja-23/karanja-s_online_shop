a28c7ba3ab66cbd3259eef34663ef161504126b9
Hosea <karanja.mungai23@gmail.com>
2025-02-03 15:26:03 +0300

    minor fix

--- client/src/Admin/AdminSignIn.jsx
+++ client/src/Admin/AdminSignIn.jsx
@@ -43,15 +43,7 @@ function AdminSignIn() {
             
         })
         .then(response => response.json())
-        .then(data => {
-            if (data.role != 'admin'){
-                console.log(data)
-                setIsNotAdmin(true)
-                setTimeout(() => {
-                    setIsNotAdmin(false)
-                },1000)
-                return
-            }
+
             if(data['token']){
                 setToken(data['token'])                
                 localStorage.setItem('token', data['token'])
